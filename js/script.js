f7c9326c65adf5814ba1e26e21996e734a37f060
Vimal Mavani <42764176+vimalmavani@users.noreply.github.com>
2022-09-04 16:35:11 +0530

    New UI
    New UI

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ $('#modal-close').click(function(){
         $('.custom-backdrop').remove();
     }
 })
+
+
+$('#lifelineBtn').click(function(e){
+    $('#LifeLineWindow').addClass('open');
+    if($('#LifeLineWindow').hasClass('open')){
+        $('.main-container').append('<div class="custom-backdrop" style="opacity:1;"></div>')
+    }
+})
+$('#LifelineClose').click(function(){
+    $('#LifeLineWindow').removeClass('open');
+    if(!$('#LifeLineWindow').hasClass('open')){
+        $('.custom-backdrop').remove();
+    }
+})
